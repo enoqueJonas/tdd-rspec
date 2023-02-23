690ca030b4947fb1807913ada63cd035229cdb78
enoqueJonas <enoque.macanda@gmail.com>
2023-02-23 18:47:19 +0200

    UPDATE solver.rb adding a #factorila method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,10 +2,8 @@ require 'rspec'
 require_relative 'spec_helper.rb'
 
 describe Solver do
-    before :each do
-        @solver = Solver.new
-    end
     describe '#factorial' do
-        facto = @solver.factorial(2)
+        solver = Solver.new
+        facto = solver.factorial(2)
     end
 end
