3970500590cc86994d86d59810e8dfecf9f5e9ef
enoqueJonas <enoque.macanda@gmail.com>
2023-02-23 18:42:18 +0200

    UPDATE solver_spec.rb creating a test for #factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,5 +2,10 @@ require 'rspec'
 require_relative 'spec_helper.rb'
 
 describe Solver do
-    
+    before :each do
+        @solver = Solver.new
+    end
+    describe '#factorial' do
+        facto = @solver.factorial(2)
+    end
 end
