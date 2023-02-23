b6a8ea6048df3811e621bd7ecf881cc0dd5fd2d3
enoqueJonas <enoque.macanda@gmail.com>
2023-02-23 18:56:23 +0200

    IMPROVE method #factorial in solver.r

--- solver.rb
+++ solver.rb
@@ -1,5 +1,8 @@
 class Solver
     def factorial(n) 
-
+        if n <= 1 
+            return 1
+        end
+        return n * factorial(n-1)
     end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,7 +3,11 @@ require_relative 'spec_helper.rb'
 
 describe Solver do
     describe '#factorial' do
-        solver = Solver.new
-        facto = solver.factorial(2)
+        it "Should return the factorial on 'n'" do
+            solver = Solver.new
+            facto = solver.factorial(2)
+
+            expect(facto).to eql 2
+        end
     end
 end
