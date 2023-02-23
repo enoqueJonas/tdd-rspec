4849f4006f25a2eb1fe6d37183b1dfb404253ea1
enoqueJonas <enoque.macanda@gmail.com>
2023-02-23 19:01:07 +0200

    IMPROVE #factorila to refuse negative numbers

--- solver.rb
+++ solver.rb
@@ -1,5 +1,9 @@
 class Solver
     def factorial(n) 
+        if n < 1 
+            return "Error: Invalid number!"
+        end
+
         if n <= 1 
             return 1
         end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,11 +3,11 @@ require_relative 'spec_helper.rb'
 
 describe Solver do
     describe '#factorial' do
-        it "Should return the factorial on 'n'" do
+        it "Should return the factorial of 'n'" do
             solver = Solver.new
-            facto = solver.factorial(2)
+            facto = solver.factorial(3)
 
-            expect(facto).to eql 2
+            expect(facto).to eql 6
         end
     end
 end
